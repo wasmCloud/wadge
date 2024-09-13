d5d6293d89bb400a0a5c05a4fe03bc210acf0c87
Roman Volosatovs <rvolosatovs@riseup.net>
2024-09-13 21:21:23 +0200

    chore: reorganize repo
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- go/west_windows_amd64.go
+++
@@ -1,3 +0,0 @@
-//go:generate cp ../target/release/libwest_sys.a ../lib/x86_64-windows/libwest.a
-
-package west
---
+++ tests/go/wasi/wasi.go
@@ -0,0 +1,64 @@
+//go:generate go run github.com/ydnar/wasm-tools-go/cmd/wit-bindgen-go@v0.1.5 generate -w service -o bindings ./wit
+
+package wasi
+
+import (
+	"log/slog"
+	"unsafe"
+
+	incominghandler "github.com/rvolosatovs/west/tests/go/wasi/bindings/wasi/http/incoming-handler"
+	"github.com/rvolosatovs/west/tests/go/wasi/bindings/wasi/http/types"
+	"github.com/ydnar/wasm-tools-go/cm"
+)
+
+func init() {
+	incominghandler.Exports.Handle = func(request types.IncomingRequest, responseOut types.ResponseOutparam) {
+		if err := handle(request, responseOut); err != nil {
+			types.ResponseOutparamSet(responseOut, cm.Err[cm.Result[types.ErrorCodeShape, types.OutgoingResponse, types.ErrorCode]](*err))
+		}
+	}
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
+
+func handle(req types.IncomingRequest, out types.ResponseOutparam) *types.ErrorCode {
+	slog.Debug("constructing new response")
+	res := types.NewOutgoingResponse(req.Headers())
+
+	slog.Debug("getting response body")
+	body := res.Body()
+	if body.IsErr() {
+		slog.Debug("failed to get body")
+		return ptr(types.ErrorCodeInternalError(cm.Some("failed to get body")))
+	}
+	bodyOut := body.OK()
+
+	slog.Debug("getting response body stream")
+	bodyWrite := bodyOut.Write()
+	if bodyWrite.IsErr() {
+		slog.Debug("failed to get response body stream")
+		return ptr(types.ErrorCodeInternalError(cm.Some("failed to get response body stream")))
+	}
+
+	slog.Debug("setting response outparam")
+	types.ResponseOutparamSet(out, cm.OK[cm.Result[types.ErrorCodeShape, types.OutgoingResponse, types.ErrorCode]](res))
+	stream := bodyWrite.OK()
+	s := "foo bar baz"
+	writeRes := stream.BlockingWriteAndFlush(cm.NewList(unsafe.StringData(s), uint(len(s))))
+	if writeRes.IsErr() {
+		slog.Error("failed to write to stream", "err", writeRes.Err())
+		return nil
+	}
+	slog.Debug("dropping body stream")
+	stream.ResourceDrop()
+
+	slog.Debug("finishing outgoing body")
+	finishRes := types.OutgoingBodyFinish(*bodyOut, cm.None[types.Fields]())
+	if finishRes.IsErr() {
+		slog.Error("failed to finish outgoing body", "err", finishRes.Err())
+		return nil
+	}
+	return nil
+}
