4479b9ec253834716ab9975de839996019bccb36
Roman Volosatovs <rvolosatovs@riseup.net>
2024-09-18 12:13:18 +0200

    feat(go-bindgen): skip `gojs` imports
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- cmd/west-bindgen-go/main.go
+++ cmd/west-bindgen-go/main.go
@@ -243,6 +243,9 @@ func generate(path string) error {
 					pos := pkg.Fset.Position(dir.Pos())
 					return fmt.Errorf("%s:%d: unexpected `go:wasmimport` directive format: %s", pos.Filename, pos.Line, dir.Text)
 				}
+				if instance == "gojs" {
+					continue
+				}
 
 				var callArgs []ast.Expr
 				appendArg := func(arg *ast.Field) error {
